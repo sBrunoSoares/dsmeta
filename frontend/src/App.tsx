4962e78b6835b09aaa8c1cf7af23e0f13b11586a
Bruno Soares <www.bruno@live.com>
2022-11-15 12:30:38 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-  <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
-  
+
 }
 
 export default App
